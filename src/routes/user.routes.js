2cb4df90d9b5c0c8be55185de27a3dbe8a8d0257
MelvinAguilar <00067621@uca.edu.sv>
2023-11-06 15:01:35 -0600

    Update: user router files

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,29 @@
 const {
+  authentication,
+  authorization,
+} = require("../middleware/auth.middleware");
+
+const {
   getAllUser,
   getUserById,
   createUser,
   updateUser,
   deleteUser,
+  singin,
 } = require("../controller/user.controller");
+
 const validateUserInput = require("../validator/user.validator");
+
+const applicationAdm = process.env.APPLICATION_ADM || "";
+const applicationUser = process.env.APPLICATION_USR || "";
+const applicationSuperAdm = process.env.APPLICATION_SDM || "";
 
 const routes = [
   {
     method: "GET",
     url: "/api/users",
     handler: getAllUser,
+    middleware: [authentication, authorization([applicationAdm])],
   },
   {
     method: "GET",
@@ -23,6 +35,11 @@ const routes = [
     url: "/api/users",
     handler: createUser,
     middleware: [validateUserInput],
+  },
+  {
+    method: "POST",
+    url: "/api/users/singin",
+    handler: singin,
   },
   {
     method: "PUT",
